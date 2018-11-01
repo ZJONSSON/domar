16bc5f4388a0b95f98b720696ca873e05936bf7e
Páll Hilmarsson <pallih@gogn.in>
2018-11-01 20:50:55 +0000

    Add other declensions of the weekdays that are in the source

--- splitter.py
+++ splitter.py
@@ -2,12 +2,13 @@ import os
 
 filename = 'hdomar_1931-1932.txt'
 
-days = ['Mánudaginn', 'Þriðjudaginn', 'Miðvikudaginn', 'Fimmtudaginn', 'Föstudaginn', 'Laugardaginn', 'Sunnudaginn']
+days = ['Mánudaginn', 'Þriðjudaginn', 'Miðvikudaginn', 'Fimmtudaginn', 'Föstudaginn', 'Laugardaginn', 'Sunnudaginn', 'Mánudag', 'Þriðjudag', 'Miðvikudag', 'Fimmtudag', 'Föstudag']
 
 with open(filename) as f:
     domur = ""
     collect = False
     for count, line in enumerate(f):
+        # TODO make this into a regex that takes into account the date
         if any(day in line for day in days):
             if domur != "":
                 with open(os.path.join('out', str(count) + ".txt"), "w") as out:
