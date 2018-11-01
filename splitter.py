9480ed5ebd72b3ed223957d2886e8c41048abbab
Páll Hilmarsson <pallih@gogn.in>
2018-11-01 16:32:26 +0000

    init

---
+++ splitter.py
@@ -0,0 +1,22 @@
+import os
+
+filename = 'hdomar_1931-1932.txt'
+
+days = ['Mánudaginn', 'Þriðjudaginn', 'Miðvikudaginn', 'Fimmtudaginn', 'Föstudaginn', 'Laugardaginn', 'Sunnudaginn']
+
+with open(filename) as f:
+    domur = ""
+    collect = False
+    for count, line in enumerate(f):
+        if any(day in line for day in days):
+            if domur != "":
+                with open(os.path.join('out', str(count) + ".txt"), "w") as out:
+                    out.write(domur)
+            domur = ""
+            domur = line
+            collect = True
+        if collect is True:
+            domur = domur + line
+    with open(str(count) + ".txt", "w") as out:
+        out.write(domur)
+
