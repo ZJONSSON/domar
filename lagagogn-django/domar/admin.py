08628ee15b5b6883501ade1b83a80755a59f37cc
Páll Hilmarsson <pallih@gogn.in>
2018-11-23 14:13:50 +0000

    Show more fields in admin for Dómar

--- lagagogn-django/domar/admin.py
+++ lagagogn-django/domar/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Domstoll, Domur
 
+
+class DomurAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'domstoll', 'date', 'tags')
+
 admin.site.register(Domstoll)
-admin.site.register(Domur)
+admin.site.register(Domur, DomurAdmin)
