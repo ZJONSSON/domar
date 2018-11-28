cd3b52839dd9ec96911415134b79f4cada782539
Páll Hilmarsson <pallih@gogn.in>
2018-11-28 10:47:10 +0000

    Display verdict count in dómur admin list view

--- lagagogn-django/domar/admin.py
+++ lagagogn-django/domar/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from .models import Domstoll, Domur
 
@@ -7,5 +8,17 @@ class DomurAdmin(admin.ModelAdmin):
     list_display = ('identifier', 'domstoll', 'date', 'tags')
     list_filter = ('domstoll',)
 
-admin.site.register(Domstoll)
+class DomstollAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        qs = super(DomstollAdmin, self).get_queryset(request)
+        return qs.annotate(domar_count=Count('domur'))
+
+    def domar_count(self, inst):
+        return inst.domar_count
+
+    list_display = ('name', 'domar_count')
+    list_filter = ('name',)
+
+admin.site.register(Domstoll, DomstollAdmin)
 admin.site.register(Domur, DomurAdmin)
