1ecc169fb30a3a05f78efecaf9c6056c3a193239
Páll Hilmarsson <pallih@gogn.in>
2018-12-03 16:59:48 +0000

    work on landsrettur scraper

--- scrapers/scrapers/spiders/landsrettur.py
+++ scrapers/scrapers/spiders/landsrettur.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapers.items import DomurItem
-from domar.models import Domstoll
+from domar.models import Domstoll, Domur
 from scrapy.http.request import Request
 import datetime
 from scrapy.exceptions import CloseSpider
 import lxml.html
-from lxml.html.clean import Cleaner
-import html2text
-from scrapers.utils import parse_icelandic_date, split_on_uppercase
+from scrapers.utils import parse_sentences, save_judgement_pdf_file
 import requests
+import io
+from wand.image import Image
+from google.cloud import vision
+from google.cloud.vision import types
+
 
 
 class LandsretturSpider(scrapy.Spider):
@@ -24,9 +27,9 @@ class LandsretturSpider(scrapy.Spider):
         r = requests.get(url)
         root = lxml.html.fromstring(r.text)
         keywords_script = root.xpath("//script[contains(text(),'KeywordsList')]")[0].text
-        keywords_script = keywords_script.replace('window.KeywordsList = [', '').replace('];', '').replace('\n','')
+        keywords_script = keywords_script.replace('window.KeywordsList = [', '').replace('];', '').replace('\n', '')
         keywords = keywords_script.split(',')
-        keywords = set([keyword.replace('"','').strip() for keyword in keywords])
+        keywords = set([keyword.replace('"', '').strip() for keyword in keywords])
 
 
     def __init__(self, offset=0, count=10, margin=30):
@@ -40,6 +43,8 @@ class LandsretturSpider(scrapy.Spider):
         self.end = self.latest_date - datetime.timedelta(days=self.margin)
         self.overview_url = 'https://landsrettur.is/default.aspx?pageitemid=4468cca6-a82f-11e5-9402-005056bc2afe&offset={}&count={}'
         self.base_url = 'https://www.landsrettur.is'
+
+        self.client = vision.ImageAnnotatorClient()
 
 
     def start_requests(self):
@@ -55,6 +60,16 @@ class LandsretturSpider(scrapy.Spider):
         root = lxml.html.fromstring(response.text)
         rows = root.xpath('//div[@class="row"]/div[@class="col-md-6 col-xs-12"]')
         for row in rows:
+            url = row.xpath('div[@class="sentence"]/a[@class="casenumber"]')[0]
+            item = DomurItem()
+            item['url'] = self.base_url + url.attrib['href']
+            identifier_tag = url.xpath('h2')[0]
+            item['identifier'] = identifier_tag.text
+            if Domur.objects.filter(identifier=item['identifier']).exists():
+                # Already seen this and saved. Nothing more to do.to
+                self.logger.info('Already seen: {}'.format(item['identifier']))
+                continue
+
             item_date = row.xpath('div[@class="sentence"]/time')[0].attrib['datetime']
             item_date_object = datetime.datetime.strptime(item_date, '%d.%m.%Y %H:%M:%S').date()
             if self.end <= item_date_object <= self.latest_date:
@@ -63,14 +78,8 @@ class LandsretturSpider(scrapy.Spider):
             else:
                 # too far, let's get out of here
                 raise CloseSpider('Reached date range: {} days'.format(self.margin))
-            item = DomurItem()
             item['date'] = item_date_object
             item['domstoll'] = self.domstoll
-            url = row.xpath('div[@class="sentence"]/a[@class="casenumber"]')[0]
-            item['url'] = self.base_url + url.attrib['href']
-            identifier_tag = url.xpath('h2')[0]
-            item['identifier'] = identifier_tag.text
-
             try:
                 parties_tag = url.xpath('p')[0]
                 # remove the \n linebreaks
@@ -87,41 +96,70 @@ class LandsretturSpider(scrapy.Spider):
             try:
                 tags_tag = row.xpath('div[@class="sentence"]/small')[0]
                 tags_text = tags_tag.text
-                #pos = [i for i, e in enumerate(tags_text + 'A') if e.isupper()]
-                #tags = [tags_text[pos[j]:pos[j + 1]].strip().rstrip('.') for j in range(len(pos) - 1)]
-                tags = split_on_uppercase(tags_text, True)
-                #tags = [tag.strip().rstrip('.') for tag in tags]
+                tags = parse_sentences(tags_text)
+                # remove the end sentence punctuation
+                tags = [tag.strip().rstrip('.') for tag in tags]
                 item['tags'] = tags
             except AttributeError:
                 # no tags
                 pass
-            print(item)
-        #     yield Request(item['url'], callback=self.parse_judgement,
-        #                   meta={'item': item})
-        # # are there rows?
-        # if len(rows) > 0:
-        #     offset = offset + 10
-        #     print(self.base_url.format(offset, count))
-        #     yield Request(self.overview_url.format(offset, count),
-        #               meta={'offset': offset, 'count': count},
-        #               callback=self.parse_overview)
+            yield Request(item['url'], callback=self.find_pdf_link,
+                           meta={'item': item})
+        # do we have a continue button? If so, let's scrape further
+        more_button = root.find_class('moreVer')
+        if more_button:
+            offset = offset + count
+            yield Request(self.overview_url.format(offset, count),
+                      meta={'offset': offset, 'count': count},
+                      callback=self.parse_overview)
 
-    def parse_judgement(self, response):
+    def find_pdf_link(self, response):
         item = response.meta['item']
         root = lxml.html.fromstring(response.text)
+        pdf_link = root.xpath('//a[contains(@class, "pdflink")]')[0]
+        pdf_url = self.base_url + pdf_link.attrib['href']
+        yield Request(pdf_url, callback=self.parse_pdf,
+                           meta={'item': item})
 
-        tags_tag = root.xpath('//div[@class="verdict-keywords"]/ul/li[@class="keyword"]')
-        tags = [tag.text for tag in tags_tag]
-        item['tags'] = tags
-        cleaner = Cleaner()
-        cleaner.kill_tags = ['title']
-        text_maker = html2text.HTML2Text()
-        text_maker.unicode_snob = True
-        text_maker.body_width = 0
-        text_maker.ignore_anchors = True
-        text_maker.ignore_emphasis = True
-        text_tag = root.xpath('//div[@id="main"]/div[@class="subpagewrapper padding20"]/div[@class="no-anchors"]')[0]
-        text_tag = cleaner.clean_html(text_tag)
-        item['text'] = text_maker.handle(lxml.html.tostring(text_tag).decode("utf-8"))
+    def parse_pdf(self, response):
+        item = response.meta['item']
+        save_judgement_pdf_file(response, str(self.domstoll))
+        f = io.BytesIO(response.body)
+        all_pages = Image(blob=f, resolution=300)
+        text = ""
+        for img in all_pages.sequence:
+            img_page = Image(image=img)
+            img_page.format = 'png'
+            output = io.BytesIO()
+            img_page.save(file=output)
+            image = types.Image(content=output.getvalue())
+            response = self.client.document_text_detection(image=image)
+            text = text + response.text_annotations[0].description
+
+
+        # read_pdf = PyPDF2.PdfFileReader(f)
+        # number_of_pages = read_pdf.getNumPages()
+        # text = ""
+        # for i in range(0, number_of_pages):
+        #     page = read_pdf.getPage(i)
+        #     page_content = page.extractText()
+        #     text = text + page_content
+
+
+        item['text'] = text
         yield item
+        # tags_tag = root.xpath('//div[@class="verdict-keywords"]/ul/li[@class="keyword"]')
+        # tags = [tag.text for tag in tags_tag]
+        # item['tags'] = tags
+        # cleaner = Cleaner()
+        # cleaner.kill_tags = ['title']
+        # text_maker = html2text.HTML2Text()
+        # text_maker.unicode_snob = True
+        # text_maker.body_width = 0
+        # text_maker.ignore_anchors = True
+        # text_maker.ignore_emphasis = True
+        # text_tag = root.xpath('//div[@id="main"]/div[@class="subpagewrapper padding20"]/div[@class="no-anchors"]')[0]
+        # text_tag = cleaner.clean_html(text_tag)
+        # item['text'] = text_maker.handle(lxml.html.tostring(text_tag).decode("utf-8"))
+        # yield item
 
